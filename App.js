67d670316dbb17006ebb3b1869be2a1856b14b08
github <zzhandlyf@gmail.com>
2018-05-05 15:47:59 +0800

    no message

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import {
+    TouchableOpacity,
+    View,
+    Text,
     StyleSheet,
     WebView,
     NativeModules
@@ -13,21 +16,38 @@ export default class App extends Component{
             case "callPhone":
                 NativeModules.NativeMethodModule.callPhone(jData.phone)
                 break;
+            case "getContact":
+                let data={
+                    name:"张三",
+                    phone:"110"
+                }
+                this.webView.postMessage(JSON.stringify(data))
+                break
         }
+    }
+    a(){
+        this.webView.postMessage("dialog("+44+")")
     }
     render() {
         return (
-            <WebView
-                automaticallyAdjustContentInsets={true}
-                javaScriptEnable={true}
-                scalesPageToFit={true}
-                domStorageEnable={true}
-                decelerationRate={"normal"}
-                startInLoadingState={true}
-                onMessage={this.onMessage.bind(this)}
-                source={require("./src/A.html")}
-            >
-            </WebView>
+            /*<View style={{flex:1,backgroundColor:"green"}}>*/
+                /*<TouchableOpacity onPress={this.a.bind(this)}>*/
+                /*<Text  style={{height:100,width:100}}>rn调用h5</Text>*/
+                /*</TouchableOpacity>*/
+                <WebView
+                    ref={(w)=>{this.webView=w}}
+                    automaticallyAdjustContentInsets={true}
+                    javaScriptEnable={true}
+                    // injectedJavaScript={"document.addEventListener('message', function(e) {eval(e.data);})"}
+                    scalesPageToFit={true}
+                    domStorageEnable={true}
+                    decelerationRate={"normal"}
+                    startInLoadingState={true}
+                    onMessage={this.onMessage.bind(this)}
+                    source={require("./src/A.html")}
+                >
+                </WebView>
+            // </View>
         );
     }
 }
